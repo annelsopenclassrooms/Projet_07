879413395da4226a33f2ee662a319d31828d358f
Anne <eris2501@gmail.com>
2024-12-30 18:00:41 +0100

    knackpack dynamic testé et ok renvoie profit

--- bruteforce3.py
+++ bruteforce3.py
@@ -2,6 +2,9 @@ import csv
 import pprint
 
 import itertools
+import time
+
+start_time = time.time()
 
 # Remplace 'fichier.csv' par le chemin de ton fichier CSV
 nom_fichier = "actions3.csv"
@@ -22,11 +25,53 @@ for dict in liste_dict:
     profit = int(profit.strip('%'))
     dict['Bénéfice (après 2 ans)'] = profit
 
-liste = ["A", "B", "C"]
+# liste = ["A", "B", "C"]
+
+# comb = []
+# for n in range(1,len(liste_dict)+1):
+#     comb.append([i for i in itertools.combinations(liste_dict,n)])
+
+# print(comb)
+# print(len(comb))
+
 
 comb = []
-for n in range(1,len(liste)+1):
-    comb.append([i for i in itertools.combinations(liste,n)])
+for n in range(1, len(liste_dict) + 1):
+    for i in itertools.combinations(liste_dict, n):
+        sum = 0
+        earning = 0
+        dict_comb = {}
+        for j in i:
+            earning = earning + (int(j['Coût par action (en euros)']) * int(j['Bénéfice (après 2 ans)'])/ 100)
+            sum = sum + int(j['Coût par action (en euros)'])
+            #print(j['Coût par action (en euros)'])
+        # print(i)
+        print(sum)
+        # print(f"earning: {earning}")
+        if sum <= 500:
+            #print("added")
+            dict_comb["combinaison"] = i
+            dict_comb["earning"] = earning
+            dict_comb["sum"] = sum
+            comb.append(dict_comb)
 
-print(comb)
-print(len(comb))
+        # else:
+        #     #print('not added')
+
+# print(len(comb))
+# for i in comb:
+#     print(i["combinaison"])
+#     print(i["earning"])
+#     print(i["sum"])
+
+# Trier la liste de dictionnaires par "earning" en ordre décroissant
+comb_sorted = sorted(comb, key=lambda x: x["earning"], reverse=True)
+
+print (comb_sorted[0]["combinaison"])
+print (comb_sorted[0]["earning"])
+
+
+end_time = time.time()
+
+execution_time = end_time - start_time
+print(f"Temps d'exécution : {execution_time:.6f} secondes")
